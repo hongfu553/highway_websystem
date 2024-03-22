3344610d3b0f8fc4244d8bd7ade3c4da290bb3c6
hongfu553 <148576720+hongfu553@users.noreply.github.com>
2024-03-22 12:07:28 +0800

    mqtt one server test

--- mqtt_test_tools/send_msg.py
+++ mqtt_test_tools/send_msg.py
@@ -1,9 +1,15 @@
 import paho.mqtt.client as mqtt
 
+broker='hongfu553.myds.me'
+port=1883
+topic='tofu/road'
+#user='48losv5973'
+#passwd='3469fgjoxy'
 #server info
 client = mqtt.Client()
-client.connect("hongfu553.myds.me", 1883)
+#client.username_pw_set(username=user,password=passwd)
+client.connect(broker,port)
 
-client.publish("tofu/road", "hello world by KenChou")
+client.publish("topic", "hello world by tofu")
 
 client.disconnect()
--- mqtt_test_tools/sub_topic.py
+++ mqtt_test_tools/sub_topic.py
@@ -1,22 +1,24 @@
 import paho.mqtt.client as mqtt
 
 # server info
-MQTT_BROKER = "hongfu553.myds.me"  # ip
-MQTT_PORT = 1883  
-MQTT_TOPIC = "tofu/road"  # topic
-
+mqtt_broker = "hongfu553.myds.me"  # ip
+mqtt_port = 1883  
+mqtt_topic = "tofu/road"  # topic
+#mqtt_user= "48losv5973"
+#mqtt_password="3469fgjoxy"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe(MQTT_TOPIC)
+    client.subscribe(mqtt_topic)
 
 def on_message(client, userdata, msg):
     print("Received message: "+msg.payload.decode())
 
 client = mqtt.Client()
+#client.username_pw_set(username=mqtt_user, password=mqtt_password)
 
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
+client.connect(mqtt_broker, mqtt_port, 60)
 client.loop_forever()
